d1bfbf6b6f3cb0ce990e49804053e162221648bf
Gabriel Vieira <gabriel.vieira@gmail.com>
2014-08-12 21:42:33 -0300

    First release

--- lib/Dist/Zilla/PluginBundle/Author/GABRIEL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/GABRIEL.pm
@@ -9,6 +9,66 @@ use Dist::Zilla;
 
 with 'Dist::Zilla::Role::PluginBundle::Easy';
 
+=head1 SYNOPSIS
+ 
+    # in dist.ini
+    [@Author::GABRIEL]
+ 
+=head1 DESCRIPTION
+ 
+	[NameFromDirectory]
+	[VersionFromModule]
+
+	[MetaResources]
+	repository.type   = git
+	repository.url    = git://github.com/gabrielmad/${dist}
+	repository.web    = http://github.com/gabrielmad/${dist}
+	bugtracker.web    = http://github.com/gabrielmad/${dist}/issues
+	bugtracker.mailto = bug-${dist}@rt.cpan.org
+	homepage          = http://metacpan.org/release/${dist}
+
+	[GatherDir]
+	include_dotfiles = 1
+
+	[PruneCruft]
+	except = ^\.travis.yml
+
+	[Git::CommitBuild]
+	release_branch  = build/%b
+	release_message = Release build of v%v (on %b)
+
+	[ReadmeFromPod]
+	[ReadmeAnyFromPod / ReadmePodInRoot]
+	type = pod
+	filename = README.pod
+
+	[ChangelogFromGit]
+	exclude_message = (\.pod|commit|Travis|POD|forgot|typo|dist.ini|branch)
+
+	[MinimumPerl]
+	[AutoPrereqs]
+
+	[PodWeaver]
+	[PodCoverageTests]
+	[PodSyntaxTests]
+
+	[ManifestSkip]
+	[MetaYAML]
+	[License]
+	[Readme]
+	[InstallGuide]
+	[ExtraTests]
+	[ExecDir]
+	[ShareDir]
+	[MakeMaker]
+	[Manifest]
+
+	[TestRelease]
+	[ConfirmRelease]
+	[UploadToCPAN]
+
+=cut 
+
 sub configure {
 	my $self = shift;
 	$self->add_plugins(
@@ -16,12 +76,12 @@ sub configure {
 		'VersionFromModule',
 		[
 			MetaResources => {
-				'repository.type'   => 'git'
-				'repository.url'    => 'git://github.com/gabrielmad/${dist}'
-				'repository.web'    => 'http://github.com/gabrielmad/${dist}'
-				'bugtracker.web'    => 'http://github.com/gabrielmad/${dist}/issues'
-				'bugtracker.mailto' => 'bug-${dist}@rt.cpan.org'
-				'homepage'          => 'http://metacpan.org/release/${dist}'
+				'repository.type'   => 'git',
+				'repository.url'    => 'git://github.com/gabrielmad/${dist}',
+				'repository.web'    => 'http://github.com/gabrielmad/${dist}',
+				'bugtracker.web'    => 'http://github.com/gabrielmad/${dist}/issues',
+				'bugtracker.mailto' => 'bug-${dist}@rt.cpan.org',
+				'homepage'          => 'http://metacpan.org/release/${dist}',
 			}
 		],
 		[ GatherDir  => { include_dotfiles => 1     }],
@@ -62,5 +122,8 @@ sub configure {
 
 	return;
 }
+
+=for HTML <a href="https://travis-ci.org/gabrielmad/Dist-Zilla-PluginBundle-Author-GABRIEL"><img src="https://travis-ci.org/gabrielmad/Dist-Zilla-PluginBundle-Author-GABRIEL.svg?branch=build%2Fmaster"></a>
+=cut
 
 1;
