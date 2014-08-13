eb5c90740331b8bf70d8d4d4c9011908a85d66df
Gabriel Vieira <gabriel.vieira@gmail.com>
2014-08-12 21:39:59 -0300

    Build results of fb78c1a (on master)

--- lib/Dist/Zilla/PluginBundle/Author/GABRIEL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/GABRIEL.pm
@@ -9,6 +9,7 @@ use Dist::Zilla;
 
 with 'Dist::Zilla::Role::PluginBundle::Easy';
 
+
 sub configure {
 	my $self = shift;
 	$self->add_plugins(
@@ -63,6 +64,7 @@ sub configure {
 	return;
 }
 
+
 1;
 
 __END__
@@ -75,9 +77,69 @@ __END__
 
 Dist::Zilla::PluginBundle::Author::GABRIEL - Dist::Zilla configurantion for GABRIEL's projects
 
+=for HTML <a href="https://travis-ci.org/gabrielmad/Dist-Zilla-PluginBundle-Author-GABRIEL"><img src="https://travis-ci.org/gabrielmad/Dist-Zilla-PluginBundle-Author-GABRIEL.svg?branch=build%2Fmaster"></a>
+
 =head1 VERSION
 
 version 0.01
+
+=head1 SYNOPSIS
+
+    # in dist.ini
+    [@Author::GABRIEL]
+
+=head1 DESCRIPTION
+
+	[NameFromDirectory]
+	[VersionFromModule]
+
+	[MetaResources]
+	repository.type   = git
+	repository.url    = git://github.com/gabrielmad/${dist}
+	repository.web    = http://github.com/gabrielmad/${dist}
+	bugtracker.web    = http://github.com/gabrielmad/${dist}/issues
+	bugtracker.mailto = bug-${dist}@rt.cpan.org
+	homepage          = http://metacpan.org/release/${dist}
+
+	[GatherDir]
+	include_dotfiles = 1
+
+	[PruneCruft]
+	except = ^\.travis.yml
+
+	[Git::CommitBuild]
+	release_branch  = build/%b
+	release_message = Release build of v%v (on %b)
+
+	[ReadmeFromPod]
+	[ReadmeAnyFromPod / ReadmePodInRoot]
+	type = pod
+	filename = README.pod
+
+	[ChangelogFromGit]
+	exclude_message = (\.pod|commit|Travis|POD|forgot|typo|dist.ini|branch)
+
+	[MinimumPerl]
+	[AutoPrereqs]
+
+	[PodWeaver]
+	[PodCoverageTests]
+	[PodSyntaxTests]
+
+	[ManifestSkip]
+	[MetaYAML]
+	[License]
+	[Readme]
+	[InstallGuide]
+	[ExtraTests]
+	[ExecDir]
+	[ShareDir]
+	[MakeMaker]
+	[Manifest]
+
+	[TestRelease]
+	[ConfirmRelease]
+	[UploadToCPAN]
 
 =head1 AUTHOR
 
